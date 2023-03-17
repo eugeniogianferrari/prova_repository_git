9e5b324d8f1cfba10e36d7339fb3f0aba02eb4e1
eugeniogianferrari <eugi2799@gmail.com>
2023-03-17 20:30:22 +0100

    first attempt at committing

---
+++ prova c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, c;
+
+    printf("Choose two integers: ");
+    scanf("%d,%d", &a, &c);
+
+    printf("Sum of those two integers: %d", a+c);
+
+    return 0;
+}
